d637c6bcf69bc2f3596ecdc9d36932b62cd97291
Danston <thedanevans@gmail.com>
2018-01-07 11:58:39 -0800

    Set up the repo/dependencies

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+};
